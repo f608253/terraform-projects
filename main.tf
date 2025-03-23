699ec4bca87a00640e36fc1ee1993a5ff0ce91e1
Manvendra Singh Yadav <34604538+f608253@users.noreply.github.com>
2025-03-23 15:19:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "local_file" "games" {
-    filename = "/home/coder/fav_games.txt"
+    filename = "/home/coder/terraform-projects/fav_games.txt"
     content = "FIFA 2021"
     file_permission = "0754"
 }
