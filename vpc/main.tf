4158216bef4d94c7bc7d9c0729f304aa7fa969aa
Manav <manav.gcet92@outlook.com>
2025-04-06 14:32:24 +0530

    create vpc code

---
+++ vpc/main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_network" "autovpc" {
+  name = "autovpc"
+  auto_create_subnetworks = true
+}
+
+resource "google_compute_network" "customvpc" {
+  name = "customvpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "sub-singapore" {
+  name = "sub-sg"
+  network = google_compute_network.customvpc.id
+  ip_cidr_range = "10.1.0.0/24"
+  region = "asia-southeast1"
+}
+
+output "auto" {
+  value = google_compute_network.autovpc.id
+}
+
+output "custom" {
+  value = google_compute_network.customvpc.id
+}
