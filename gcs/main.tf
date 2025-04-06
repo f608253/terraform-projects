320c06448304410d9afa56982bc89a9bf64142a1
Manav <manav.gcet92@outlook.com>
2025-04-06 13:08:39 +0530

    new files

--- gcs/main.tf
+++ gcs/main.tf
@@ -1,5 +1,16 @@
 resource "google_storage_bucket" "gcs2" {
   name = "tf-bucket-from-vscode"
-  storage_class = "COLDLINE"
+  storage_class = "NEARLINE"
   location = "us-west1"
+  labels = {
+    "env" = "dev-learning"
+    "dep" = "dbbuk"
+  }
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "picture" {
+  name = "My first image"
+  bucket = google_storage_bucket.gcs2.name
+  source = "IMG20250331223028.jpg"
 }
