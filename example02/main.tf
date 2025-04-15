75273f0f7bea577d31e32431071607c8a9d999b9
Manav <manav.gcet92@outlook.com>
2025-04-15 15:43:58 +0530

    Writing Content to a File

---
+++ example02/main.tf
@@ -0,0 +1,16 @@
+variable "file_name" {
+  description = "Name of the file to be created"
+  type        = string
+  default     = "example.txt"
+}
+
+variable "file_content" {
+  description = "Content to write into the file"
+  type        = string
+  default     = "Hello, Terraform!"
+}
+
+resource "local_file" "example" {
+  filename = var.file_name
+  content  = var.file_content
+}
