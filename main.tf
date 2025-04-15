5b5f37de4ed6d63348834426bd7e3302b34c80d0
Manav <manav.gcet92@outlook.com>
2025-04-15 14:37:20 +0530

    variables entries

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "devops" {
-  filename = "devopsfile.txt"
-  content = "This is from Terraform file"
+  filename = var.filename
+  content = var.content
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "filename" {
+  default = "devops-automated.txt"
+}
+
+variable "content" {
+  default = "This is coming from variables.tf file"
+}
