25cc7e5cbb8d6a04160e9ecc56bcecb79d46794f
Manav <manav.gcet92@outlook.com>
2025-04-20 16:35:02 +0530

    main.tf file

--- main.tf
+++ main.tf
@@ -9,5 +9,22 @@ terraform {
 
 provider "google" {
   project = "extended-cable-455910-g2"
-  
+
+}
+
+resource "google_compute_instance" "terraform" {
+  project = "extended-cable-455910-g2"
+  name = "terraform"
+  machine_type = "n1-standard-1"
+  zone = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
 }
